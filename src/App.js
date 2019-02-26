eb4dc74998fdd6bc877458eaf08191adf0142a2a
Your Name <wamontgomery0068@gmail.com>
2019-02-26 06:51:44 -0600

    You are able to type and have it show on the DOM

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      message: ""
+    }
+  };
+
+  handleChange( value ){
+    this.setState( {message: value})
+  };
+
   render() {
     return (
       <div className="App">
+      <input onChange={ (e) => this.handleChange( e.target.value ) } type="text" />
+      <p> {this.state.message}</p>
       </div>
     );
   }
